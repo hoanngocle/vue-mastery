826b49657adce23dae9c9e785b8ae511e8649016
NyxDarkness <hoanngocle.93@gmail.com>
2022-05-23 18:41:13 +0700

    Add lesson 2

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-const girl = "Ikaros";
+const girl = 'Ikaros'
+
+const app = Vue.createApp({
+    data() {
+        return {
+            girl: 'Ikaros',
+            description:
+                'Ikaros is the sole heir of the renowned Mage family of Penglai Island.',
+        }
+    },
+})
